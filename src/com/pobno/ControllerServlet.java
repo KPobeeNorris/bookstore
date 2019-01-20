333ed4f83c9f663f46031d6949f12bef86b1b74c
Kath Pobee-Norris <kpobeenorris@gmail.com>
2019-01-20 11:35:51 +0000

    Configure project to use maven
    Enabled dependencies and created pom.xml file.
    Created ArrayList of books and displayed them with the help of jsp tags

--- src/com/pobno/ControllerServlet.java
+++ src/com/pobno/ControllerServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 /**
  * Servlet implementation class ControllerServlet
@@ -15,19 +16,23 @@ import java.io.PrintWriter;
 
 public class ControllerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private ArrayList<String> bookTitles = new ArrayList<String>();
        
     /**
      * @see HttpServlet#HttpServlet()
      */
     public ControllerServlet() {
         super();
+        bookTitles.add("Lord of the Files");
+        bookTitles.add("A Tale of Two Servers");
+        bookTitles.add("To Catch a Throw");
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setAttribute("book_title", "1984");
+		request.setAttribute("book_titles", bookTitles);
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/BookList.jsp");
 		dispatcher.forward(request, response);
 		
