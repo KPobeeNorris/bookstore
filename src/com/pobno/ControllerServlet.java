b5824f5befbc196748af2d60b8682b046a3f8982
Kath Pobee-Norris <kpobeenorris@gmail.com>
2019-01-20 08:35:28 +0000

    Create BookList.jsp and forward title attribute to it

--- src/com/pobno/ControllerServlet.java
+++ src/com/pobno/ControllerServlet.java
@@ -1,8 +1,9 @@
 package com.pobno;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,28 +21,22 @@ public class ControllerServlet extends HttpServlet {
      */
     public ControllerServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-//		PrintWriter output = response.getWriter();
-//		
-//		String title = request.getParameter("title");
-//		String author = request.getParameter("author");
-//		
-//		output.println("Book Title: " + title);
-//		output.println("Author: " + author);
+		request.setAttribute("book_title", "1984");
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/BookList.jsp");
+		dispatcher.forward(request, response);
+		
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		PrintWriter output = response.getWriter();
 		
 		String title = request.getParameter("title");
